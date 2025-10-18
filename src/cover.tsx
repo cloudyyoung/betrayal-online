ae3e459713e2555cd5ce78dc7ff9f45aecf2b476
Yunfan Yang <cloudy.young@outlook.com>
2025-10-18 00:28:45 -0600

    Update logo assets and improve cover layout
    Replaced the logo image with a cropped version and updated references in the cover component. Improved layout and styling for better alignment and responsiveness on the cover page.

--- src/cover.tsx
+++ src/cover.tsx
@@ -24,26 +24,26 @@ const BetrayalCover = () => {
 
     return (
         <div className='h-screen bg-[url("/bg-light-big.webp")] bg-repeat bg-cover bg-center overflow-hidden'>
-            <div className='relative h-screen max-w-2xl mx-auto p-4 gap-8'>
-                <div className='flex flex-col items-center justify-center h-screen gap-4 -mt-10'>
+            <div className='relative h-screen max-w-2xl mx-auto gap-8 px-6'>
+                <div className='flex flex-col justify-center items-start sm:items-center h-screen gap-4 -mt-8'>
                     <img
-                        className='w-full'
-                        src="/betrayal_logo.png"
+                        className='w-xs -ml-1.5 sm:w-10/12 sm:ml-0'
+                        src="/betrayal-logo-cropped.png"
                         alt="logo"
                     />
-                    <div className='font-tomarik-brush text-lg text-zinc-800 -mt-10'>
-                        An unofficial scripted online web version
+                    <div className='font-tomarik-brush text-yellow-900 text-lg sm:text-2xl'>
+                        Unofficial, scripted online web version
                     </div>
                     <div className='flex flex-row gap-4 mt-6'>
                         <Link to="/play">
-                            <Button className='bg-yellow-700 text-white font-tomarik-brush md:text-xl px-6 py-4 hover:bg-yellow-600'>Create New Game</Button>
+                            <Button className='bg-yellow-700 text-white font-tomarik-brush sm:text-xl px-6 py-4 hover:bg-yellow-600'>Create New Game</Button>
                         </Link>
                         <Link to="/play">
-                            <Button className='bg-white/80 text-amber-700 font-tomarik-brush md:text-xl px-6 py-4 hover:bg-white/100'>Join Existing</Button>
+                            <Button className='bg-white/80 text-amber-700 font-tomarik-brush sm:text-xl px-6 py-4 hover:bg-white/100'>Join Existing</Button>
                         </Link>
                     </div>
                 </div>
-                <div className='text-zinc-700 italic text-xs tracking-tighter leading-3 sticky bottom-0 left-0 right-0 pb-4'>
+                <div className='text-zinc-700 italic text-xs tracking-tighter leading-3 sticky bottom-0 left-0 right-0 pb-6'>
                     Disclaimer: This is an unofficial, fan-made version of Betrayal at the House on the Hill (3rd Edition), created for personal and educational use only.
                     All rights belong to Avalon Hill and Hasbro, Inc.
                     This project is not affiliated with or endorsed by either company.
