c547f30fe97bf60784e9768752f035c1d8776110
Yunfan Yang <cloudy.young@outlook.com>
2025-10-21 21:25:41 -0600

    Update matches.tsx

--- src/view/matches.tsx
+++ src/view/matches.tsx
@@ -29,7 +29,7 @@ export default function MatchesList() {
         <CoverContainer>
             <div className='bg-red-800/10 p-6 space-y-5 my-12'>
                 <div className='flex items-center justify-between mb-6'>
-                    <h1 className='text-3xl font-tomarik-brush text-red-900/85'>Available Match</h1>
+                    <h1 className='text-3xl font-tomarik-brush text-red-900/85'>Available Matches</h1>
                     <Button onClick={loadMatches} className='bg-amber-700 text-white px-4 py-2 hover:bg-amber-600'>Refresh</Button>
                 </div>
 
