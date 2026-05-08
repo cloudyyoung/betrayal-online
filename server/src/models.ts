d8466e73989b78bb52eabede4bde353fa3b4fc71
Yunfan Yang <cloudy.young@outlook.com>
2026-05-08 10:35:57 -0600

    Update models.ts

--- server/src/models.ts
+++ server/src/models.ts
@@ -13,19 +13,19 @@ const accountSchema = new Schema<Account>({
 
 export const AccountModel: Model<Account> = mongoose.model<Account>('Account', accountSchema);
 
-export interface MOtp extends Document {
+export interface Otp extends Document {
     email: string;
     code: string;
     createdAt: Date;
 }
 
-const otpSchema = new Schema<MOtp>({
+const otpSchema = new Schema<Otp>({
     email: { type: String, required: true, index: true },
     code: { type: String, required: true },
     createdAt: { type: Date, default: Date.now, expires: 600 }, // TTL: 10 minutes
 });
 
-export const OtpModel: Model<MOtp> = mongoose.model<MOtp>('Otp', otpSchema);
+export const OtpModel: Model<Otp> = mongoose.model<Otp>('Otp', otpSchema);
 
 export type MGame = Omit<Game, 'id' | 'isPasswordProtected' | 'createdAt'> & Document & {
     password?: string;
